4b662757b17a6fb6cad3fb31adf1229508bd94a8
sealabr <fabio.mendes.viana@gmail.com>
2017-08-15 00:22:38 -0300

    be happy

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/tts', function(req, res){
+	var msg = req.query.msg;
+	var delay = req.query.delay;
+	io.emit('chat message', msg, delay );
+	res.end('ok');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+little socket.io to process voice messsages
+
+$ npm install express
+$ npm install socket.io
+
+open to listen
+
+http://localhost:3000/
+
+open another tab to send tts
+
+http://localhost:3000/tts?msg=mensagem&delay=20
+
+this api is in portuguese brazil.
